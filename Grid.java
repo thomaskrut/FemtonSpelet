7aaa915f246c3bb685a0b6cf22d03c1e43a2e20f
Thomas Sjöberg <thomas.sjoberg91@gmail.com>
2022-10-29 21:26:28 +0200

    la till actionListener på knapparna. Försöker komma på ett bra sätt att flytta knapparna.

--- Grid.java
+++ Grid.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class Grid extends JFrame {
+public class Grid extends JFrame implements ActionListener {
     JFrame frame = new JFrame();
     JPanel mainPanel = new JPanel(); //Ändrar namn till mainPanel för att lättare ha koll på vad koden referar till.
     JButton[][] buttonArray;
@@ -54,11 +56,13 @@ public class Grid extends JFrame {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 buttonArray[i][j] = new JButton();
+                buttonArray[i][j].addActionListener(this);              //Lägger till action listener på knappen när den skapas.
                 mainPanel.add(buttonArray[i][j]);
             }
         }
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
+
                 buttonArray[i][j].setText(String.valueOf(oneDimensionalArray[(i * columns) + j]));
             }
         }
@@ -67,7 +71,19 @@ public class Grid extends JFrame {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        if(s.isBlank()){
+            s="0";
+        }
+        int selectedTile =Integer.parseInt(s);
+
+        System.out.println(s);
+        System.out.println(selectedTile);
+        //används i testsyfte då jag inte kan skapa en testmap. Skriver ut button text.
+        //Kanske gå att använda mot int array "gameBoard" för att ha koll vart varje tile är lättare?
 
 
-
+    }
 }
