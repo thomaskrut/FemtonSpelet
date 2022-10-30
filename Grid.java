26600864844aceda3c87aeb4069215a2dbef7950
Thomas Krut <thomas.k.krut@gmail.com>
2022-10-30 13:16:45 +0100

    test av arraylist för slumpmässig ordning på siffrorna

--- Grid.java
+++ Grid.java
@@ -2,27 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
 
 
-public class Grid extends JFrame implements ActionListener {
+public class Grid implements ActionListener {
     JFrame frame = new JFrame();
     JPanel mainPanel = new JPanel(); //Ändrar namn till mainPanel för att lättare ha koll på vad koden referar till.
     JButton[][] buttonArray;
     final int rows = 4;  //Horizontal
     final int columns = 4;//Vertikal
 
-    int[] oneDimensionalArray = generateOneDimensionalArray(); //Generar en 1d array med elementen 0-15.
+    // int[] oneDimensionalArray = generateOneDimensionalArray(); //Generar en 1d array med elementen 0-15.
+    List<Integer> listOfNumbers = generateListOfNumbers();
+    int[][] gameBoard = generateBoardArray(listOfNumbers); //Skapar 'spelbrädan'. todo:Klarar vi oss med att bara Jbutton arrayen? Ska denna raderas?
 
     public Grid() {
-        int[][] gameBoard = generateBoardArray(oneDimensionalArray); //Skapar 'spelbrädan'. todo:Klarar vi oss med att bara Jbutton arrayen? Ska denna raderas?
+
         showGrid();
     }
-
 
     public void showGrid() {
 
         mainPanel.setLayout(new GridLayout(4, 4));
-        buttonArray = generateButtonArray(oneDimensionalArray); //Skapar buttonarray och tilldelar textvärde 0-15.
+        mainPanel.setBackground(Color.black);
+        buttonArray = generateButtonArray(); //Skapar buttonarray och tilldelar textvärde 0-15.
         frame.add(mainPanel);
         frame.setVisible(true);
         frame.setSize(600, 600);
@@ -32,41 +36,49 @@ public class Grid extends JFrame implements ActionListener {
     }
 
 
-
-    public int[] generateOneDimensionalArray() {  //Skapar en array 0-15. Används för att skapa gameBoard och buttonArray.
+    // används inte i denna variant
+    /* public int[] generateOneDimensionalArray() {  //Skapar en array 0-15. Används för att skapa gameBoard och buttonArray.
         int[] generatedArray = new int[16];
         for (int i = 0; i < 16; i++) {
             generatedArray[i] = i;
         }
         return generatedArray;
+    } */
+
+    public List<Integer> generateListOfNumbers() {
+        List<Integer> listOfNumbers = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            listOfNumbers.add(i);
+        }
+        Collections.shuffle(listOfNumbers);
+        return listOfNumbers;
+
     }
 
 
-    public int[][] generateBoardArray(int[] oneDimensionalArray) {
+    public int[][] generateBoardArray(List<Integer> listOfNumbers) {
         int[][] gameBoardArray = new int[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++)
-                gameBoardArray[i][j] = oneDimensionalArray[(i * columns) + j];
+                gameBoardArray[i][j] = listOfNumbers.get((i * columns) + j);
         }
         return gameBoardArray;
     }
 
-    public JButton[][] generateButtonArray(int[] oneDimensionalArray) {
+    public JButton[][] generateButtonArray() {
         JButton[][] buttonArray = new JButton[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 buttonArray[i][j] = new JButton();
                 buttonArray[i][j].addActionListener(this);              //Lägger till action listener på knappen när den skapas.
+                buttonArray[i][j].setText(String.valueOf(gameBoard[i][j]));
+                if (gameBoard[i][j] == 0) {
+                    buttonArray[i][j].setVisible(false);
+                }
                 mainPanel.add(buttonArray[i][j]);
             }
         }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
 
-                buttonArray[i][j].setText(String.valueOf(oneDimensionalArray[(i * columns) + j]));
-            }
-        }
-        buttonArray[0][0].setText(""); // 0 0 blir blank istället för 0.
         return buttonArray;
     }
 
